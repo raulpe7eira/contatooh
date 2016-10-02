10f161579202540aef7c8eca481a3772187485c3
Raul Pereira <raul.pereira@gmail.com>
2016-10-01 21:09:33 -0300

    adjust global env

--- config/config.js
+++ config/config.js
@@ -2,8 +2,6 @@
 
 module.exports = function() {
 
-	console.log('./env/' + process.env.NODE_ENV + ":" + ('./env/' + process.env.NODE_ENV).length);
-
-	return require('./env/' + process.env.NODE_ENV);
+	return require('./env/' + process.env.NODE_ENV + ".js");
 
 };
