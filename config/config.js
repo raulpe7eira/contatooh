05cf9eb1d44a832dfe162cb9ea719aa47754c7e7
Raul Pereira <raul.pereira@gmail.com>
2016-10-01 21:24:12 -0300

    config cache for travis and appveyor

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 
 module.exports = function() {
 
-	return require('./env/' + process.env.NODE_ENV + ".js");
+	return require('./env/' + process.env.NODE_ENV);
 
 };
--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,6 @@ module.exports = function() {
 
 	var Usuario = mongoose.model('Usuario');
 
-	console.log(config.clientID);
-
 	// configura a estratégia de autenticação
 	passport.use(new GitHubStrategy({
 		clientID: config.clientID,
